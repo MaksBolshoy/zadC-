8507985e740659be68023dabfa1437e2580f4e18
MaksBolshoy <118996271+MaksBolshoy@users.noreply.github.com>
2022-12-14 14:28:41 +0300

    задачи 25-29

---
+++ zad25/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+/*
+Console.WriteLine("Введите число А: ");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите число В: ");
+int b =Convert.ToInt32(Console.ReadLine());
+int n =0;
+while (n<=b)
+{
+    Console.WriteLine(Math.Pow(a, b));
+   break;
+   
+}
+*/
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine()!);
+int Getstep(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+       result*= a;
+      
+    }
+    return result;
+
+}
+Console.WriteLine(Getstep(a, b));
---
+++ zad27/Program.cs
@@ -0,0 +1,59 @@
+﻿//Задача 27: Напишите функцию и запустите ее, которая
+//принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+// 82-> 10
+//9012 ->12
+/*
+int NumberSum(int a)
+{
+    int sum = 0;
+    for (i = 1; i <= a.Length; i++)
+    {
+        sum += a.Lenght;
+        i++;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(NumberSum(a));
+*/
+/*
+int Count(int A)
+{
+    int res = 0;
+    while (A!= 0)
+    {
+        A = A / 10;
+        res+=A;
+    }
+    return res;
+}
+Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(Count(A));
+*/
+/*
+int Multi(int A)
+{
+    int sum =1;
+    for( int i =1; i<=A; i++)
+    {
+        sum =sum+i;
+        i++;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число А: ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(Multi(N));
+*/
+
+Console.WriteLine("Введите число А: ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+int a1=N%10;
+int a2=N/10%10;
+int a3=N/100%10;
+int a4=N/1000;
+N=a1+a2+a3+a4;
+Console.Write(N);
