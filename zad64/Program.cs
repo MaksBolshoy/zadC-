803daa2fb8585ea89bb03a50cb494d27580d15bf
MaksBolshoy <118996271+MaksBolshoy@users.noreply.github.com>
2022-12-29 18:47:23 +0300

    64:66.

---
+++ zad64/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+/*
+Console.Write("Введите число  ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i =1;
+RevArray(N,i);
+void RevArray(int N, int i)
+{
+    if(N>i)
+    {
+        return;
+    }
+
+    else
+    {
+        RevArray (N, i  +  1 );
+        Console. WriteLine ( i +  "  " );
+    }
+}
+*/
+
+Console. WriteLine ( " Введите натуральное число больше 1: " );
+int n =  Convert.ToInt32( Console.ReadLine ( )) ;
+
+
+void  NumberCounter ( int n )
+{
+    if ( n <  0 ) Console. WriteLine( $" { n } не натуральное число " );
+    if ( n  ==  0 ) return;// вернуть ;
+    Console . Write ( " {0,1} " , n );
+     NumberCounter ( n  -  1 );
+}
+
+ NumberCounter ( n );
+
+
+
+//int PrintRevArray( int N)
+//{
+ //for(int i = N; i > 0; i--)
+     //  Console.Write(i + " ");
+///}
+//Console.WriteLine( PrintRevArray(int N));
+//PrintRevArray( int N);
+//public static void Main(String[] args)
+//{
+    //int N = 5;
+ 
+  //  PrintRevArray(N);
+//}
---
+++ zad66/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+ 
+Console. Write ( " Введите число M: " );
+int  m  =  Convert. ToInt32 ( Console.ReadLine ( ) ) ;
+
+Console . Write ( " Введите число N: " );
+int  n  =  Convert . ToInt32 ( Console.ReadLine ( ) ) ;
+
+SumMNRT ( m , n );
+
+// вызов функции "сумма чисел от M до N"
+void SumMNRT ( int  m , int  n )
+{
+    Console . Write ( SumMN ( m  -  1 , n ));
+}
+
+// функция колличества чисел от M до N
+int  SumMN ( int  m , int  n )
+{
+   //int  n =1 ;
+    if ( m  ==  n )
+        return  0 ;
+    else
+    {
+        int res=m;
+        m ++ ;
+        res  =  m  +  SumMN ( m , n );
+        return  res ;
+    }
+}
+//SumMNRT ( int  m , int  n );
+//SumMN ( int  m , int  n );
+//SumMN ( res);
